e624da63af9752d722711bdae633c1f82c3ad8ac
Sean Duiser <duzer84@gmail.com>
2022-03-24 20:46:40 -0400

    seeing if this app.post works

--- server.js
+++ server.js
@@ -22,47 +22,59 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
+    const myNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+    const parseMyNotes = JSON.parse(myNotes);
 
-    const { title, text } = req.body;
+    req.body.id = uuid();
+    parseMyNotes.push(req.body);
 
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-            review_id: uuid(),
-        };
-
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-            } else {
-                const parsedNotes = JSON.parse(data);
-
-                parsedNotes.push(newNote);
-
-                fs.writeFile(
-                    './db/db.json',
-                    JSON.stringify(parsedNotes, null, 4),
-                    (writeErr) =>
-                        writeErr
-                            ? console.error(writeErr)
-                            : console.info('Successfully updated notes!')
-                );
-            }
-        });
-
-        const response = {
-            status: 'success',
-            body: newNote,
-        };
-
-        console.log(response);
-        res.status(201).json(response);
-    } else {
-        res.status(500).json('Error in posting note');
-    }
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parseMyNotes), "utf-8");
+    res.json("You have successfully added a note");
 });
+
+// couldn't figure out how to get the below code to properly function...
+// app.post('/api/notes', (req, res) => {
+//     console.info(`${req.method} request received to add a note`);
+
+//     const { title, text } = req.body;
+
+//     if (title && text) {
+//         const newNote = {
+//             title,
+//             text,
+//             review_id: uuid(),
+//         };
+
+//         fs.readFile('./db/db.json', 'utf8', (err, data) => {
+//             if (err) {
+//                 console.error(err);
+//             } else {
+//                 const parsedNotes = JSON.parse(data);
+
+//                 parsedNotes.push(newNote);
+
+//                 fs.writeFile(
+//                     './db/db.json',
+//                     JSON.stringify(parsedNotes, null, 4),
+//                     (writeErr) =>
+//                         writeErr
+//                             ? console.error(writeErr)
+//                             : console.info('Successfully updated notes!')
+//                 );
+//             }
+//         });
+
+//         const response = {
+//             status: 'success',
+//             body: newNote,
+//         };
+
+//         console.log(response);
+//         res.status(201).json(response);
+//     } else {
+//         res.status(500).json('Error in posting note');
+//     }
+// });
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
