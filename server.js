c8586b5bfd2aaa932cc4b1ee75fbecb99fe4ee08
Sean Duiser <duzer84@gmail.com>
2022-03-24 20:22:11 -0400

    restarting PC

--- server.js
+++ server.js
@@ -22,33 +22,25 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    // Log that a POST request was received
     console.info(`${req.method} request received to add a note`);
 
-    // Destructuring assignment for the items in req.body
     const { title, text } = req.body;
 
-    // If all the required properties are present
     if (title && text) {
-        // Variable for the object we will save
         const newNote = {
             title,
             text,
             review_id: uuid(),
         };
 
-        // Obtain existing reviews
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err);
             } else {
-                // Convert string into JSON object
                 const parsedNotes = JSON.parse(data);
 
-                // Add a new review
                 parsedNotes.push(newNote);
 
-                // Write updated reviews back to the file
                 fs.writeFile(
                     './db/db.json',
                     JSON.stringify(parsedNotes, null, 4),
